a0e5dc9abb246ccdd7a968d7818d2840fa1e4507
takayuki.seki <seki@omnetwork.jp>
2016-10-20 12:00:26 +0900

    [update]バージョン5.0リリース
    ①Gradle2.2.2に対応
    ②バックグラウンド稼働のスイッチを廃止
    ③停止時にGmailでメール送信スイッチの初期値をOFFに変更
    ④停止時にCSVファイルを削除スイッチの初期値をOFFに変更
    ⑤直線加速度センサーの廃止
    ⑥方位センサーの出力単位をDegree⇒Radianに変更しDegree角表示エリアを削除
    ⑦その他搭載されているセンサーの一覧を削除
    ⑧各センサーの値を少数第三位切り捨てに変更
    ⑨保存ファイル名の入力欄を追加
    ⑩保存ファイル名の初期化ボタンを追加
    ⑪画面をスリープ状態にしない考慮の追加

--- app/src/main/java/jp/gr/niguniken/MainActivity.java
+++ app/src/main/java/jp/gr/niguniken/MainActivity.java
@@ -8,9 +8,11 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -40,12 +42,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private Sensor mGyroscope;
     private Sensor mMagnetic_Field;
     private Sensor mGravity;
-    private Sensor mLinear_Acceleration;
 
     // センサーの利用可否
     private boolean mAccelerometer_Available = false;
     private boolean mGravity_Available = false;
-    private boolean mLinear_Acceleration_Available = false;
     private boolean mGyroscope_Available = false;
     private boolean mMagnetic_Field_Available = false;
 
@@ -56,38 +56,32 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     // 方位計算用
     private static final int MATRIX_SIZE = 16;
-    private float[] mRotationMatrix_In = new float[MATRIX_SIZE];
-    private float[] mRotationMatrix_Out = new float[MATRIX_SIZE];
-    private float[] mInclinationMatrix = new float[MATRIX_SIZE];
+    private float[] mOrientation_RotationMatrix_In = new float[MATRIX_SIZE];
+    private float[] mOrientation_RotationMatrix_Out = new float[MATRIX_SIZE];
+    private float[] mOrientation_InclinationMatrix = new float[MATRIX_SIZE];
 
     // 画面コントロール
+    TextView txtFileName;
     TextView txtNow;
+    TextView txtOrientation_Azimuth_Radian;
+    TextView txtOrientation_Pitch_Radian;
+    TextView txtOrientation_Roll_Radian;
     TextView txtAccelerometer_X;
     TextView txtAccelerometer_Y;
     TextView txtAccelerometer_Z;
     TextView txtGravity_X;
     TextView txtGravity_Y;
     TextView txtGravity_Z;
-    TextView txtLinear_Acceleration_X;
-    TextView txtLinear_Acceleration_Y;
-    TextView txtLinear_Acceleration_Z;
     TextView txtGyroscope_X;
     TextView txtGyroscope_Y;
     TextView txtGyroscope_Z;
     TextView txtMagnetic_Field_X;
     TextView txtMagnetic_Field_Y;
     TextView txtMagnetic_Field_Z;
-    TextView txtOrientation_Azimuth;
-    TextView txtOrientation_Pitch;
-    TextView txtOrientation_Roll;
-    TextView txtOrientation_Azimuth_Radian;
-    TextView txtOrientation_Pitch_Radian;
-    TextView txtOrientation_Roll_Radian;
 
     // CSV出力タイマー
     private Timer mTimer;
     private TimerTask mTimerTask = null;
-    private Handler mHandler = new Handler();
 
     // 現在作成中のCSVファイルパス
     private String mCSVFilePath;
@@ -100,6 +94,9 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Keep screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         // 画面のコントロールを取得
         getView();
 
@@ -109,13 +106,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         // センサーマネージャーからセンサーオブジェクトを取得
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mGravity = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
-        mLinear_Acceleration = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         mGyroscope = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mMagnetic_Field = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
 
         // CSV保存パスを画面に表示（起動前は参考情報）
         TextView txt = (TextView)findViewById(R.id.txtFilePath);
-        txt.setText(Environment.getExternalStorageDirectory().getPath() + "/uniken_<yyyymmddhhmmss>.csv");
+        txt.setText(Environment.getExternalStorageDirectory().getPath() + "/uniken_<入力ファイル名>_<yyyymmddhhmmss>.csv");
 
         // センサー一覧を取得し、使用可能なセンサーに「利用可能」を表記
         List<Sensor> lstSensor = mSensorManager.getSensorList(Sensor.TYPE_ALL);
@@ -145,26 +141,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     mGravity_Available = true;
                     ((TextView)findViewById(R.id.txtGravity_Available)).setText(R.string.sensor_available_ok);
                     break;
-
-                case Sensor.TYPE_LINEAR_ACCELERATION:
-                    // 直線加速度センサーあり
-                    mLinear_Acceleration_Available = true;
-                    ((TextView)findViewById(R.id.txtLinear_Acceleration_Available)).setText(R.string.sensor_available_ok);
-                    break;
-
-                default:
-                    // その他のセンサーは一覧化しておく
-                    strOther_Sensor.append("[" + sensor.getName() + "]");
-                    break;
             }
         }
-        // その他のセンサーを表示
-        if (strOther_Sensor.length() > 0) ((TextView)findViewById(R.id.txtOther_Sensor)).setText(strOther_Sensor.toString());
 
         if (mGravity_Available & mMagnetic_Field_Available){
             // 重力センサーと磁界センサーが使用可能な場合には、方位センサーも使用可能
             ((TextView)findViewById(R.id.txtOrientation_Available)).setText(R.string.sensor_available_ok);
         }
+
+        // 初期化ボタンのクリックイベントを定義
+        Button btnFileName_Initialize = (Button)findViewById(R.id.btnFileName_Initialize);
+        btnFileName_Initialize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 初期化ボタンが押され場合、ファイル名を初期値に戻す
+                txtFileName.setText(R.string.file_name_default);
+            }
+        });
 
         // トグルボタンのクリックイベントを定義
         ToggleButton btn = (ToggleButton)findViewById(R.id.tglStart);
@@ -185,7 +178,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     DateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
 
                     // CSV保存パス作成　SDカードにuniken_<タイムスタンプ>.csv
-                    mCSVFilePath = Environment.getExternalStorageDirectory().getPath() + "/uniken_" + formatter.format(now) + ".csv";
+                    mCSVFilePath = Environment.getExternalStorageDirectory().getPath() + "/uniken_" + txtFileName.getText() + "_" + formatter.format(now) + ".csv";
 
                     // CSV保存パスを画面に表示
                     TextView txt = (TextView)findViewById(R.id.txtFilePath);
@@ -194,8 +187,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                     try {
                         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(mCSVFilePath, false), "SJIS"));
                         // csvファイルにヘッダー作成
-//                        bw.write("日時,加速度X,加速度Y,加速度Z,重力加速度X,重力加速度Y,重力加速度Z,直線加速度X,直線加速度Y,直線加速度Z,角速度X,角速度Y,角速度Z,磁界X,磁界Y,磁界Z,方位角,傾斜角,回転角");
-                        bw.write("datetime,X_acceleration,Y_acceleration,Z_acceleration,X_gravity,Y_gravity,Z_gravity,X_linear_acceleration,Y_linear_acceleration,Z_linear_acceleration,X_angular_velocity,Y_angular_velocity,Z_angular_velocity,X_magnetic_field,Y_magnetic_field,Z_magnetic_field,azimuth,pitch,roll");
+                        bw.write("datetime,X_acceleration,Y_acceleration,Z_acceleration,X_gravity,Y_gravity,Z_gravity,X_angular_velocity,Y_angular_velocity,Z_angular_velocity,X_magnetic_field,Y_magnetic_field,Z_magnetic_field,azimuth,pitch,roll");
                         bw.newLine();
                         bw.flush();
                         bw.close();
@@ -269,12 +261,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 bw.write(",");
                 bw.write(txtGravity_Z.getText().toString());
                 bw.write(",");
-                bw.write(txtLinear_Acceleration_X.getText().toString());
-                bw.write(",");
-                bw.write(txtLinear_Acceleration_Y.getText().toString());
-                bw.write(",");
-                bw.write(txtLinear_Acceleration_Z.getText().toString());
-                bw.write(",");
                 bw.write(txtGyroscope_X.getText().toString());
                 bw.write(",");
                 bw.write(txtGyroscope_Y.getText().toString());
@@ -287,11 +273,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                 bw.write(",");
                 bw.write(txtMagnetic_Field_Z.getText().toString());
                 bw.write(",");
-                bw.write(txtOrientation_Azimuth.getText().toString());
+                bw.write(txtOrientation_Azimuth_Radian.getText().toString());
                 bw.write(",");
-                bw.write(txtOrientation_Pitch.getText().toString());
+                bw.write(txtOrientation_Pitch_Radian.getText().toString());
                 bw.write(",");
-                bw.write(txtOrientation_Roll.getText().toString());
+                bw.write(txtOrientation_Roll_Radian.getText().toString());
                 bw.newLine();
                 bw.flush();
                 bw.close();
@@ -315,28 +301,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     // 画面の出力コントロールを取得
     private void getView(){
+        txtFileName                     = (TextView)findViewById(R.id.txtFileName);
         txtNow                           = (TextView)findViewById(R.id.txtDateTime);
+        txtOrientation_Azimuth_Radian = (TextView)findViewById(R.id.txtOrientation_Azimuth_Radian);
+        txtOrientation_Pitch_Radian   = (TextView)findViewById(R.id.txtOrientation_Pitch_Radian);
+        txtOrientation_Roll_Radian    = (TextView)findViewById(R.id.txtOrientation_Roll_Radian);
         txtAccelerometer_X             = (TextView)findViewById(R.id.txtAccelerometer_X);
         txtAccelerometer_Y             = (TextView)findViewById(R.id.txtAccelerometer_Y);
         txtAccelerometer_Z             = (TextView)findViewById(R.id.txtAccelerometer_Z);
         txtGravity_X                    = (TextView)findViewById(R.id.txtGravity_X);
         txtGravity_Y                    = (TextView)findViewById(R.id.txtGravity_Y);
         txtGravity_Z                    = (TextView)findViewById(R.id.txtGravity_Z);
-        txtLinear_Acceleration_X     = (TextView)findViewById(R.id.txtLinear_Acceleration_X);
-        txtLinear_Acceleration_Y     = (TextView)findViewById(R.id.txtLinear_Acceleration_Y);
-        txtLinear_Acceleration_Z     = (TextView)findViewById(R.id.txtLinear_Acceleration_Z);
         txtGyroscope_X                  = (TextView)findViewById(R.id.txtGyroscope_X);
         txtGyroscope_Y                  = (TextView)findViewById(R.id.txtGyroscope_Y);
         txtGyroscope_Z                  = (TextView)findViewById(R.id.txtGyroscope_Z);
         txtMagnetic_Field_X            = (TextView)findViewById(R.id.txtMagnetic_Field_X);
         txtMagnetic_Field_Y            = (TextView)findViewById(R.id.txtMagnetic_Field_Y);
         txtMagnetic_Field_Z            = (TextView)findViewById(R.id.txtMagnetic_Field_Z);
-        txtOrientation_Azimuth         = (TextView)findViewById(R.id.txtOrientation_Azimuth);
-        txtOrientation_Pitch           = (TextView)findViewById(R.id.txtOrientation_Pitch);
-        txtOrientation_Roll            = (TextView)findViewById(R.id.txtOrientation_Roll);
-        txtOrientation_Azimuth_Radian = (TextView)findViewById(R.id.txtOrientation_Azimuth_Radian);
-        txtOrientation_Pitch_Radian   = (TextView)findViewById(R.id.txtOrientation_Pitch_Radian);
-        txtOrientation_Roll_Radian    = (TextView)findViewById(R.id.txtOrientation_Roll_Radian);
     }
 
     @Override
@@ -377,17 +358,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                 case Sensor.TYPE_ACCELEROMETER:
                     // 加速度センサーの場合
-                    txtAccelerometer_X.setText(String.valueOf(event.values[0]));
-                    txtAccelerometer_Y.setText(String.valueOf(event.values[1]));
-                    txtAccelerometer_Z.setText(String.valueOf(event.values[2]));
+                    txtAccelerometer_X.setText(String.valueOf(Math.floor(event.values[0] * 100) / 100));
+                    txtAccelerometer_Y.setText(String.valueOf(Math.floor(event.values[1] * 100) / 100));
+                    txtAccelerometer_Z.setText(String.valueOf(Math.floor(event.values[2] * 100) / 100));
                     break;
 
                 case Sensor.TYPE_MAGNETIC_FIELD:
                     // 磁界センサーの場合
                     mMagnetic_Field_Values = event.values.clone();
-                    txtMagnetic_Field_X.setText(String.valueOf(event.values[0]));
-                    txtMagnetic_Field_Y.setText(String.valueOf(event.values[1]));
-                    txtMagnetic_Field_Z.setText(String.valueOf(event.values[2]));
+                    txtMagnetic_Field_X.setText(String.valueOf(Math.floor(event.values[0] * 100) / 100));
+                    txtMagnetic_Field_Y.setText(String.valueOf(Math.floor(event.values[1] * 100) / 100));
+                    txtMagnetic_Field_Z.setText(String.valueOf(Math.floor(event.values[2] * 100) / 100));
 
                     // 磁界センサーの場合は、方位センサー再計算
                     calcOrientation = true;
@@ -395,41 +376,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                 case Sensor.TYPE_GYROSCOPE:
                     // 角速度センサーの場合
-                    txtGyroscope_X.setText(String.valueOf(event.values[0]));
-                    txtGyroscope_Y.setText(String.valueOf(event.values[1]));
-                    txtGyroscope_Z.setText(String.valueOf(event.values[2]));
+                    txtGyroscope_X.setText(String.valueOf(Math.floor(event.values[0] * 100) / 100));
+                    txtGyroscope_Y.setText(String.valueOf(Math.floor(event.values[1] * 100) / 100));
+                    txtGyroscope_Z.setText(String.valueOf(Math.floor(event.values[2] * 100) / 100));
                     break;
 
                 case Sensor.TYPE_GRAVITY:
                     // 重力センサーの場合
                     mGravity_Values = event.values.clone();
-                    txtGravity_X.setText(String.valueOf(event.values[0]));
-                    txtGravity_Y.setText(String.valueOf(event.values[1]));
-                    txtGravity_Z.setText(String.valueOf(event.values[2]));
+                    txtGravity_X.setText(String.valueOf(Math.floor(event.values[0] * 100) / 100));
+                    txtGravity_Y.setText(String.valueOf(Math.floor(event.values[1] * 100) / 100));
+                    txtGravity_Z.setText(String.valueOf(Math.floor(event.values[2] * 100) / 100));
 
                     // 重力センサーの場合は、方位センサー再計算
                     calcOrientation = true;
-                    break;
-
-                case Sensor.TYPE_LINEAR_ACCELERATION:
-                    // 直線速度センサーの場合
-                    txtLinear_Acceleration_X.setText(String.valueOf(event.values[0]));
-                    txtLinear_Acceleration_Y.setText(String.valueOf(event.values[1]));
-                    txtLinear_Acceleration_Z.setText(String.valueOf(event.values[2]));
                     break;
             }
 
             if (mAccelerometer_Available & mMagnetic_Field_Available & calcOrientation){
                 // 方位センサー計算
-                SensorManager.getRotationMatrix(mRotationMatrix_In, mInclinationMatrix, mGravity_Values, mMagnetic_Field_Values);
-                SensorManager.remapCoordinateSystem(mRotationMatrix_In, SensorManager.AXIS_X, SensorManager.AXIS_Y, mRotationMatrix_Out);
-                SensorManager.getOrientation(mRotationMatrix_Out, mOrientation_Values);
-                txtOrientation_Azimuth.setText(String.valueOf(Math.floor(Math.toDegrees(mOrientation_Values[0]))) + "°");
-                txtOrientation_Pitch.setText(String.valueOf(Math.floor(Math.toDegrees(mOrientation_Values[1]))) + "°");
-                txtOrientation_Roll.setText(String.valueOf(Math.floor(Math.toDegrees(mOrientation_Values[2]))) + "°");
-                txtOrientation_Azimuth_Radian.setText(String.valueOf(mOrientation_Values[0]));
-                txtOrientation_Pitch_Radian.setText(String.valueOf(mOrientation_Values[1]));
-                txtOrientation_Roll_Radian.setText(String.valueOf(mOrientation_Values[2]));
+                SensorManager.getRotationMatrix(mOrientation_RotationMatrix_In, mOrientation_InclinationMatrix, mGravity_Values, mMagnetic_Field_Values);
+                SensorManager.remapCoordinateSystem(mOrientation_RotationMatrix_In, SensorManager.AXIS_X, SensorManager.AXIS_Y, mOrientation_RotationMatrix_Out);
+                SensorManager.getOrientation(mOrientation_RotationMatrix_Out, mOrientation_Values);
+
+                float Azimuth_Radian = mOrientation_Values[0];
+                txtOrientation_Azimuth_Radian.setText(String.valueOf(Math.floor(Azimuth_Radian * 100) / 100));
+                float Pitch_Radian = mOrientation_Values[1];
+                txtOrientation_Pitch_Radian.setText(String.valueOf(Math.floor(Pitch_Radian * 100) / 100));
+                float Roll_Radian = mOrientation_Values[2];
+                txtOrientation_Roll_Radian.setText(String.valueOf(Math.floor(Roll_Radian * 100) / 100));
+
+                // 参考：Degree角への変換
+//                float Azimuth_Degree = (float)Math.floor(Math.toDegrees(Azimuth_Radian));
+//                float Pitch_Degree = (float)Math.floor(Math.toDegrees(Pitch_Radian));
+//                float Roll_Degree = (float)Math.floor(Math.toDegrees(Roll_Radian));
             }
         }
     }
@@ -448,23 +428,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         if (mGyroscope_Available)  mSensorManager.registerListener(this, mGyroscope, mSensorManager.SENSOR_DELAY_NORMAL);
         if (mMagnetic_Field_Available)  mSensorManager.registerListener(this, mMagnetic_Field, mSensorManager.SENSOR_DELAY_NORMAL);
         if (mGravity_Available)  mSensorManager.registerListener(this, mGravity, mSensorManager.SENSOR_DELAY_NORMAL);
-        if (mLinear_Acceleration_Available)  mSensorManager.registerListener(this, mLinear_Acceleration, mSensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-
-        // バックグラウンド稼働スイッチがONの場合は、停止しない
-        Switch swh = (Switch)findViewById(R.id.swhBackground_Running);
-
-        if (!(swh.isChecked())) {
-            // 非アクティブ時にSensorEventをとらないようにリスナの登録解除
-            if (mAccelerometer_Available)  mSensorManager.unregisterListener(this, mAccelerometer);
-            if (mGyroscope_Available)   mSensorManager.unregisterListener(this, mGyroscope);
-            if (mMagnetic_Field_Available)  mSensorManager.unregisterListener(this, mMagnetic_Field);
-            if (mGravity_Available)   mSensorManager.unregisterListener(this, mGravity);
-            if (mLinear_Acceleration_Available)  mSensorManager.unregisterListener(this, mLinear_Acceleration);
-        }
+        // バックグラウンドでも稼働し続ける
     }
 }
